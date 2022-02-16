82a72749c4174039848fc5fc1159ba804e26df5f
manuelballester <manuelballestermatito2021@u.northwestern.edu>
2022-02-16 00:42:53 -0600

    done_for_today

--- src/learning_NN.py
+++ src/learning_NN.py
@@ -1,10 +1,22 @@
 # Basic libraries 
 import torch
-import numpy as np
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
-from src.models import * 
 
-def hola(input, output):
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Hyper-parameters 
+input_size = 784 # 28x28
+hidden_size = 500 
+num_classes = 10
+num_epochs = 2
+batch_size = 100
+learning_rate = 0.001
+
+
+def network(input, output):
 
     predicted_out = output*1.2
 
